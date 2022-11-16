b202143b223494e0a0a96a7333fcbfc6bceeb55c
Ca <ca@wolffungame.com>
2022-11-16 19:00:22 +0700

    add autorotate

---
+++ Assets/FishNet/Example/All/Prediction/Transform/Scripts/AutoRotate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class AutoRotate : MonoBehaviour
+{
+    [System.Serializable]
+    private enum RotateAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private bool clockWise;
+
+    [SerializeField]
+    private RotateAxis rotateAroundAxis;
+
+    void Update()
+    {
+        int clockWiseFactor = clockWise ? -1 : 1;
+
+        float rotateAngle = clockWiseFactor * rotateSpeed * Time.deltaTime;
+
+        Vector3 rotationEuler = Vector3.zero;
+
+        switch (rotateAroundAxis)
+        {
+            case RotateAxis.X:
+                rotationEuler.x = rotateAngle;
+                break;
+            case RotateAxis.Y:
+                rotationEuler.y = rotateAngle;
+                break;
+            case RotateAxis.Z:
+                rotationEuler.z = rotateAngle;
+                break;
+        }
+
+        transform.Rotate(rotationEuler);
+    }
+}
